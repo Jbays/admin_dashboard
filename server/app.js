4efba2f3b52fef3fd48afce501d7ab7fb17dc59e
jbays <jjbaize@gmail.com>
2019-07-04 17:32:22 -0500

    Got a working connection to my mongo cluster.  Decided to pivot from postgres to mongo -- to decrease the overhead of redesigning the nonrelational schema into relational.

--- server/app.js
+++ server/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
 const PORT = process.env.PORT || 2525;
+const password = process.env.MONGOPASS;
 const ENVIRONMENT = process.env.ENVIRONMENT || 'development';
-const config = require('./knexfile')[ENVIRONMENT];
-const knex = require('knex')(config);
+
+mongoose.connect(`mongodb+srv://jaybays:${password}@swytch-db-obf3f.mongodb.net/test?retryWrites=true&w=majority`);
 
 const app = express();
+
 app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
@@ -21,3 +27,7 @@ app.use((req,res,next)=>{
 app.listen(PORT,()=>{
   console.log(`Server is listening on PORT ${PORT}`);
 });
+
+app.post('/createNewUser',(req,res)=>{
+
+})
