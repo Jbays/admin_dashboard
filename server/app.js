2260455607cabed7447e1809da79611f9ad291ea
jbays <jjbaize@gmail.com>
2019-07-07 13:38:05 -0500

    Got the local server connected to my database.  Now I'll start building out the auth routes.  That's the first step.

--- server/app.js
+++ server/app.js
@@ -1,33 +1,47 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const config = require('./config');
 
 require('dotenv').config();
 
-const PORT = process.env.PORT || 2525;
-const password = process.env.MONGOPASS;
-const ENVIRONMENT = process.env.ENVIRONMENT || 'development';
-
-mongoose.connect(`mongodb+srv://jaybays:${password}@swytch-db-obf3f.mongodb.net/test?retryWrites=true&w=majority`);
+// mongoose.connect(`mongodb+srv://jaybays:${password}@swytch-db-obf3f.mongodb.net/test?retryWrites=true&w=majority`);
 
 const app = express();
-
-app.use(morgan('combined'));
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json());
 
 app.use((req,res,next)=>{
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods","GET, POST, PUT");
   next();
-})
-
-app.listen(PORT,()=>{
-  console.log(`Server is listening on PORT ${PORT}`);
 });
 
-app.post('/createNewUser',(req,res)=>{
+const db = mongoose.connection.openUri(config.dbUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+db.on("error", () => {
+  console.error.bind(console, "# MongoDB - connection error: ")
+});
+db.once("open", () => {
+  console.log("Swytch database connection ok!")
+});
 
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((error, req, res, next) =>{
+  console.error(`error from server: ${error}`);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).join({message:message,data:data});
 })
+
+app.listen(config.port);
+
+console.log(`Server is listening on http://localhost:${config.port}`);
