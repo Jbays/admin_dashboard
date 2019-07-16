29cd7bbbe112d53e90d547367dc636e31c0273af
jbays <jjbaize@gmail.com>
2019-07-15 20:57:38 -0500

    Got auth working.  successfully created the first user according to the users model.

--- server/models/user.js
+++ server/models/user.js
@@ -1,149 +1,98 @@
-'use strict'
-var mongoose = require('mongoose')
-var Schema = mongoose.Schema
-let { formatPhoneNumber, validatePhoneNumber } = require('../libPhone')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require("bcrypt-nodejs");
 
-module.exports = function () {
-  const bcrypt = require('bcrypt')
-  let model
-  let UserSchema = new Schema({
-    first_name: { type: String, trim: true, required: false, minLength: 1 },
-    last_name: { type: String, trim: true, required: false, minLength: 1 },
-    username: {
-      type: String,
-      lowercase: true,
-      required: true,
-      trim: true,
-      unique: true
-    },
-    email: {
-      type: String,
-      trim: true,
-      lowercase: true,
-      required: true,
-      minLength: 1,
-      match: /.+\@.+\..+/,
-      unique: true
-    },
-    mobile_device_id: {
-      type: String
-    },
-    biometrics_enabled: {
-      type: Boolean
-    },
-    password: { type: String, required: false, minLength: 8 },
-    verified: { type: Boolean, required: true, default: false },
-    terms: { type: Boolean, required: false, default: false },
-    country_code: String,
-    phone_number: {
-      type: String,
-      trim: true,
-      required: true,
-      unique: true,
-      sparse: true,
-      validate: {
-        validator: validatePhoneNumber
-      },
-      set: normalizePhoneNumber,
-      get: internationalPhoneNumber
-    },
-    wallet: {
-      type: String,
-      required: true,
-      unique: true
-    },
-    token_staking: {
-      valid_token_balance: { type: Boolean },
-      checked_on: { type: Date },
-      token_hash: { type: String }
-    },
-    roles: {
-      type: [String],
-      enum: ['user', 'org.member', 'org.super-user', 'org.admin', 'super-user', 'admin', 'service-account'],
-      default: 'user',
-      index: true
-    },
-    orgs: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'org'
-    }],
-    assets: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'asset'
-    }],
-    contact: { type: mongoose.Schema.Types.Object },
-    history: [mongoose.Schema.Types.Object],
-    meta: { type: mongoose.Schema.Types.Object }
-  }, { timestamps: { created_at: 'created_at', updated_at: 'modified_at' }, collection: 'users' })
+const UserSchema = new Schema({
+  first_name: { type: String, trim: true, required: false, minLength: 1 },
+  last_name: { type: String, trim: true, required: false, minLength: 1 },
+  username: {
+    type: String,
+    trim: true,
+    required: true,
+    lowercase: true,
+    unique: true,
+    minLength:8
+  },
+  email:{
+    type: String,
+    trim: true,
+    lowercase: true,
+    required: true,
+    minLength: 1,
+    match: /.+\@.+\..+/,
+    unique: true
+  },
+  mobile_device_id: { type: String },
+  biometrics_enabled: { type: Boolean },
+  password: { type: String, required: false, minLength: 8 },
+  verified: { type: Boolean, required: true, default: false },
+  terms: { type: Boolean, required: false, default: false },
+  country_code: String,
+  phone_number: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+    sparse: true,
+  },
+  wallet: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  token_staking: {
+    valid_token_balance: { type: Boolean },
+    checked_on: { type: Date },
+    token_hash: { type: String }
+  },
+  roles: {
+    type: [String],
+    enum: ['user', 'org.member', 'org.super-user', 'org.admin', 'super-user', 'admin', 'service-account'],
+    default: 'user',
+    index: true
+  },
+  orgs: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'org'
+  }],
+  assets: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'asset'
+  }],
+  contact: { type: mongoose.Schema.Types.Object },
+  history: [mongoose.Schema.Types.Object],
+  meta: { type: mongoose.Schema.Types.Object }
+});
 
-  UserSchema.index({ username: 1 })
-  UserSchema.index({ email: 1 })
-  UserSchema.index({ roles: 1 })
-  UserSchema.index({ wallet: 1 })
-
-  UserSchema.pre('save', function (next) {
-    const user = this
-    if (!user.password) {
-      next()
+function encrypt(next) {
+  const user = this;
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
     }
 
-    if (this.isModified('password') || this.isNew) {
-      bcrypt.genSalt(10, (error, salt) => {
-        if (error) return next(error)
-        bcrypt.hash(user.password, salt, (error, hash) => {
-          if (error) return next(error)
-          user.password = hash
-          next()
-        })
-      })
-    } else {
-      return next()
-    }
-  })
-  UserSchema.methods.comparePassword = function (password, cb) {
-    bcrypt.compare(password, this.password, (error, matches) => {
-      if (error) {
-        return cb(error)
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) {
+        return next(err);
       }
-      return cb(null, matches)
-    })
-  }
-  UserSchema.methods.comparePasswordAync = function (password) {
-    let self = this
-    return new Promise(function (resolve, reject) {
-      bcrypt.compare(password, self.password, (error, matches) => {
-        if (error) {
-          return reject(error)
-        }
-        if (!matches) {
-          return reject(new Error('Invalid Password'))
-        }
-        return resolve(matches)
-      })
-    })
-  }
+      user.password = hash;
+      next();
+    });
+  });
+};
 
-  try {
-    // Throws an error if "Name" hasn't been registered
-    model = mongoose.model('user')
-  } catch (e) {
-    model = mongoose.model('user', UserSchema)
-  }
-  return model
-}
+function comparePassword(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, isMatch);
+  });
+};
 
-const normalizePhoneNumber = function (pn) {
-  return pn.replace(/[^\d]+/g, '')
-}
+UserSchema.methods.encrypt = encrypt;
+UserSchema.methods.comparePassword = comparePassword;
 
-const internationalPhoneNumber = function (pn) {
-  var cc = this.country_code || 'US'
+const User = mongoose.model("User", UserSchema);
 
-  var cleanNum = pn
-  try {
-    cleanNum = formatPhoneNumber(cleanNum, cc)
-  } catch (e) {
-    console.log(e)
-  }
-  return cleanNum
-}
+module.exports = User;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,3 +4,12 @@ const router = express.Router();
 require('../services/passport');
 const passport = require('passport');
 
+const authController = require('../controllers/auth')
+
+//protects user token generation by verifying user exists
+const requireSignin = passport.authenticate('local', { session: false })
+
+router.post("/signin", requireSignin, authController.signin);
+router.post("/signup", authController.signup);
+
+module.exports = router;
