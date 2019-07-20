081245b119e3a1156acce24d186bb6bb4bc35769
jbays <jjbaize@gmail.com>
2019-07-20 15:02:07 -0500

    Successfully created my first organization using post request from postman.

--- server/controllers/organization.js
+++ server/controllers/organization.js
@@ -1,10 +1,20 @@
+const User = require('../models/user');
 const Organization = require('../models/organization');
 
 function createNewOrganization(req,res,next){
-  console.log('createNewOrganization talking!');
-  console.log('req.body',req.body);
-
+  const userId = req.body.member.user_id;
   const name = req.body.name;
+  const description = req.body.description;
+  const profilePictureUrl = req.body.profile_picture_url;
+  const websiteUrl = req.body.website_url;
+  const orgWalletAddress = req.body.org_wallet_address;
+  const admin = req.body.UserId;
+  const defaultGroup = req.body.group;
+  const member = {user_id:req.body.UserId,role:'admin'}
+  const verified = req.body.verified;
+  const location = req.body.location;
+  // const history = req.body.history;
+  const meta = req.body.meta;
 
   if ( !name ){
     return res.status(422).send({ error: "Your organization requres a name." });
@@ -18,9 +28,43 @@ function createNewOrganization(req,res,next){
     if (existingOrg){
       return res.status(422).send({error:"That organization name is already in use.  Please choose a unique organization name"})
     } else {
+
       //create the organization instance of the mongoose model.
       //save it
       //then return the id and a nice message
+      const organization = new Organization({
+        name,
+        description,
+        profilePictureUrl,
+        websiteUrl,
+        orgWalletAddress,
+        admin,
+        defaultGroup,
+        member,
+        verified,
+        location
+      });
+
+      console.log('admin>>>>',admin);
+
+      console.log('organization',organization);
+      console.log('organization.admin',organization.admin);
+
+      organization.save((err)=>{
+        if(err){
+          return next(err);
+        }
+
+        //check if user exists
+        //if user exists, update user's roles, and orgs
+        
+
+        res.json({
+          organizationId:organization._id,
+          name:name,
+          note:'you successfully created a new device'
+        })
+      })
     }
   });
 }
@@ -28,3 +72,4 @@ function createNewOrganization(req,res,next){
 module.exports = {
   createNewOrganization
 }
+
