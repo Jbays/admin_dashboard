b1468a9dc8262ff6d1f044df169693b8d9ae5486
jbays <jjbaize@gmail.com>
2019-07-21 15:20:16 -0500

    Got organizations to be created according to the organizations model.

--- server/controllers/organization.js
+++ server/controllers/organization.js
@@ -2,13 +2,12 @@ const User = require('../models/user');
 const Organization = require('../models/organization');
 
 function createNewOrganization(req,res,next){
-  const userId = req.body.member.user_id;
   const name = req.body.name;
   const description = req.body.description;
   const profilePictureUrl = req.body.profile_picture_url;
   const websiteUrl = req.body.website_url;
   const orgWalletAddress = req.body.org_wallet_address;
-  const admin = req.body.UserId;
+  const admin = req.body.member.user_id;
   const defaultGroup = req.body.group;
   const member = {user_id:req.body.UserId,role:'admin'}
   const verified = req.body.verified;
@@ -29,9 +28,6 @@ function createNewOrganization(req,res,next){
       return res.status(422).send({error:"That organization name is already in use.  Please choose a unique organization name"})
     } else {
 
-      //create the organization instance of the mongoose model.
-      //save it
-      //then return the id and a nice message
       const organization = new Organization({
         name,
         description,
@@ -45,25 +41,39 @@ function createNewOrganization(req,res,next){
         location
       });
 
-      console.log('admin>>>>',admin);
-
-      console.log('organization',organization);
-      console.log('organization.admin',organization.admin);
-
       organization.save((err)=>{
         if(err){
           return next(err);
         }
 
-        //check if user exists
-        //if user exists, update user's roles, and orgs
-        
+        //query probably doesn't need to return all the fields (like password)
+        //but just the necessary fields.
+        return User.findOneAndUpdate(admin).exec()
+          .then((userDoc)=>{
+            let userDocRoles = userDoc.roles;
+            let userDocOrgs = userDoc.orgs;
+            if ( !userDocRoles.includes('admin') ) {
+              userDocRoles.push('admin');
+              userDoc.roles = userDocRoles;
+            }
+            
+            if ( !userDocOrgs.includes(name) ) {
+              userDocOrgs.push(organization._id);
+              userDoc.orgs = userDocOrgs;
+            }
+            userDoc.save();
 
-        res.json({
-          organizationId:organization._id,
-          name:name,
-          note:'you successfully created a new device'
-        })
+            return res.json({
+              organizationId:organization._id,
+              name:name,
+              note:'you successfully created a new device'
+            })
+
+          })
+          .catch((err)=>{
+            console.error(`error while trying to update admin user's record: ${err}`)
+          });
+
       })
     }
   });
